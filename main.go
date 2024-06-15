6c6817d986384e147a9063667f2844393a5b1dc2
makeworld <makeworld@protonmail.com>
2024-06-15 01:57:49 -0400

    use templating, use simple.css, add example homepage content

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"embed"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+//go:embed assets
+//go:embed templates
+var content embed.FS
+
+var templates = template.Must(template.ParseFS(content, "templates/*"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data any) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "pages/index.html")
+		renderTemplate(w, "index", nil)
 	})
-	http.Handle("GET /assets/", http.FileServer(http.Dir(".")))
+	http.Handle("GET /assets/", http.FileServerFS(content))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
